e1f672d0970820ae69158e25a7da8f0fd35f9db8
max-codeware <max.codeware@gmail.com>
2018-05-15 23:07:29 +0200

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo Configuring libraries
+gcc -c -o ./src/Internal/LibC/libc.o ./src/Internal/LibC/libc.c
+ar rcs ./src/Internal/LibC/libc.a ./src/Internal/LibC/libc.o
+if [ ! -d "./bin" ]; then
+    echo Creating bin folder
+    mkdir ./bin 
+fi
+if [ ! -d "/usr/lib/LinCAS" ]; then
+    echo Creating lib folder
+    sudo mkdir /usr/lib/LinCAS
+fi
+
+echo Compiling LinCAS...
+crystal build ./src/LinCAS.cr -o ./bin/lincas --no-debug
+echo Installing LinCAS...
+sudo cp ./bin/lincas /usr/bin/lincas
+echo Finished
+
