5483960fbc3ab271e172e3145e47de4ad62a8c9c
max-codeware <max.codeware@gmail.com>
2018-06-08 12:38:23 +0200

    Changed build and spec process for travis c

--- spec/LinCAS_spec.cr
+++ spec/LinCAS_spec.cr
@@ -1,64 +1,6 @@
 require "./spec_helper"
 
 describe LinCAS do 
-
-    it "Creates needed folders" do 
-        [
-        Process.run("mkdir",%w|
-            bin
-        |),
-
-        Process.run("sudo",%w|
-            mkdir
-            /usr/lib/LinCAS
-        |)
-        ].each do |res|
-            res.exit_status.should eq(0)
-        end
-    end
-
-    it "Compiles libraries" do
-
-        [
-        Process.run("gcc",%w|
-            -c
-            -o
-            src/Internal/LibC/libc.o
-            src/Internal/LibC/libc.c
-        |,output: STDOUT, input: STDIN, error: STDERR),
-
-        Process.run("ar",%w|
-            rcs
-            src/Internal/LibC/libc.a
-            src/Internal/LibC/libc.o
-        |,output: STDOUT, input: STDIN, error: STDERR)
-        
-        ].each do |res|
-            res.exit_status.should eq(0)
-        end
-    end
-
-    it "Builds LinCAS" do
-        # Building the interpreter
-        Process.run("crystal",%w|
-            build
-            ./src/LinCAS.cr
-            -o
-            ./bin/lincas
-            --no-debug
-            --release
-            --stats
-            --cross-compile
-        |,output: STDOUT, input: STDIN, error: STDERR).exit_status.should eq(0)
-    end
-
-#    it "Copies the binaries into /usr/bin folder" do
-#        Process.run("sudo",%w|
-#            cp
-#            ./bin/lincas /usr/bin/lincas
-#        |,output: STDOUT, input: STDIN, error: STDERR).exit_status.should eq(0)
-#    end
-
     it "Runs the specs" do 
         Process.run("bin/lincas",%w|
             test/LinCAS_test.lc
