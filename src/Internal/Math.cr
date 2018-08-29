eb990c773c4087ec32075bb29463136117ca3666
max-codeware <max.codeware@gmail.com>
2018-08-29 19:35:02 +0200

    Improved Math module (ary operations)

--- src/Internal/Math.cr
+++ src/Internal/Math.cr
@@ -15,221 +15,300 @@
 
 module LinCAS::Internal
 
+    macro mfun(name)
+        arg = args.as(T2)[1]
+        next lc_{{name.id}}_ary(arg) if arg.is_a? LcArray
+        next lc_{{name.id}}(arg)
+    end
+
+    macro mfun2(name)
+        args = args.as(T3)
+        arg1 = args[1]
+        arg2 = args[2]
+        if arg1.is_a? LcArray && arg2.is_a? LcArray
+            next lc_{{name.id}}_ary(arg1,arg2)
+        end
+        next lc_{{name.id}}(arg1,arg2)
+    end
+
+    macro default_def(name)
+        private def self.lc_{{name.id}}(v : Value)
+            return complex_{{name.id}}(v) if v.is_a? LcCmx
+            val = internal.lc_num_to_cr_f(v)
+            return Null unless val
+            return num2float(Math.{{name.id}}(val).to_f)
+        end
+    end
+
+    macro def_with_block(name)
+        private def self.lc_{{name.id}}(v : Value)
+            return complex_{{name.id}}(v) if v.is_a? LcCmx
+            val = internal.lc_num_to_cr_f(v)
+            return Null unless val
+            {{yield}}
+            return num2float(Math.{{name.id}}(val).to_f)
+        end
+    end
+
+    macro default_adef(name)
+        private def self.lc_a{{name.id}}(v : Value)
+            return complex_arc{{name.id}}(v) if v.is_a? LcCmx
+            val = internal.lc_num_to_cr_f(v)
+            return Null unless val
+            return num2float(Math.a{{name.id}}(val).to_f)
+        end
+    end
+
+    macro adef_with_block(name)
+        private def self.lc_a{{name.id}}(v : Value)
+            return complex_arc{{name.id}}(v) if v.is_a? LcCmx
+            val = internal.lc_num_to_cr_f(v)
+            return Null unless val
+            {{yield}}
+            return num2float(Math.a{{name.id}}(val).to_f)
+        end
+    end
+
+    {% for name in %w|cos sin acos asin tan atan cosh sinh asinh acosh gamma
+                    exp log tanh atanh sqrt cbrt log10 |%}
+        private def self.lc_{{name.id}}_ary(v : Value)
+            new_ary = build_ary_new
+            ary_iterate(v) do |obj|
+                res = lc_{{name.id}}(obj)
+                if test(res)
+                    lc_ary_push(new_ary,res)
+                else
+                    return new_ary 
+                end
+            end
+            return new_ary
+        end
+    {% end %}
+
+    {% for name in %w|atan2 nrt copysign hypot| %}
+        def self.lc_{{name.id}}_ary(a1 : Value, a2 : Value)
+            if ary_size(a1) != ary_size(a2)
+                lc_raise(LcArgumentError,"(Array size missmatch)")
+                return Null 
+            end
+            new_ary = build_ary_new
+            ary_iterate_with_index(a1) do |obj,i|
+                tmp = lc_{{name.id}}(obj,ary_at_index(a2,i))
+                if test(tmp)
+                    lc_ary_push(new_ary,tmp)
+                else
+                    return new_ary
+                end
+            end
+            return new_ary
+        end
+    {% end %}
+
+
+    default_def cos
     m_cos = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_cos(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.cos(val).to_f)
+        mfun cos
     end
 
+    default_def sin
     m_sin = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_sin(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.sin(val).to_f)
+        mfun sin
     end
 
+    default_def tan
     m_tan = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_tan(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.tan(val).to_f)
+        mfun tan
     end
 
+    adef_with_block(cos) do 
+        if val > 1
+            lc_raise(LcMathError,"(Value out of domain)")
+            return Null 
+        end
+    end
     m_acos = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_arccos(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        if val > 1
-            lc_raise(LcMathError,"(Value out of domain)")
-            next Null 
-        end 
-        next num2float(Math.acos(val).to_f)       
+        mfun acos      
     end
 
+    adef_with_block(sin) do 
+        if val > 1
+            lc_raise(LcMathError,"(Value out of domain)")
+            return Null 
+        end
+    end
     m_asin = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_arcsin(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        if val > 1
-            lc_raise(LcMathError,"(Value out of domain)")
-            next Null 
-        end 
-        next num2float(Math.asin(val).to_f) 
+        mfun asin
     end
 
+    default_adef tan
     m_atan = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_arctan(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.atan(val).to_f) 
+        mfun atan
     end
 
+    default_def sinh
     m_sinh = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_sinh(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.sinh(val).to_f)
+        mfun sinh
     end
 
+    default_def cosh
     m_cosh = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_cosh(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.cosh(val).to_f)
+        mfun cosh
     end
 
+    default_adef cosh
     m_acosh = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_arccosh(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.acosh(val).to_f)
+        mfun acosh
     end
 
+    default_adef sinh
     m_asinh = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_arcsinh(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.asinh(val).to_f)
+        mfun asinh
+    end
+    
+    def self.lc_gamma(v : Value)
+        val = internal.lc_num_to_cr_f(v)
+        return Null unless val
+        if val < 0
+            lc_raise(LcMathError,"(Value out of domain)")
+            return Null 
+        end 
+        return num2float(Math.gamma(val).to_f)
     end
 
     m_gamma = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        if val < 0
-            lc_raise(LcMathError,"(Value out of domain)")
-            next Null 
-        end 
-        next num2float(Math.gamma(val).to_f)
+        mfun gamma
     end
 
+    default_def exp
     m_exp = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_exp(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.exp(val).to_f)
+        mfun exp
     end
-
-    m_log = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_log(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
+    
+    def_with_block(log) do 
         if val < 0
             lc_raise(LcMathError,"(Value out of domain)")
-            next Null
+            return Null
         end
-        next num2float(Math.log(val).to_f)
+    end
+    m_log = LcProc.new do |args|
+        mfun log
     end
 
+    default_def tanh
     m_tanh = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_tanh(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.tanh(val).to_f)
+        mfun tanh
     end
 
-    m_atanh = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_arctanh(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
+    adef_with_block(tanh) do
         if val.abs > 1
             lc_raise(LcMathError,"(Value out of domain)")
-            next Null
+            return Null
         end
-        next num2float(Math.atanh(val).to_f)
+    end
+    m_atanh = LcProc.new do |args|
+        mfun atanh
+    end
+
+    def self.lc_atan2(v1 : Value, v2 : Value)
+        if v1.is_a? LcCmx && v2.is_a? LcCmx
+             tmp = complex_div(v1,v2)
+             return lc_atan(tmp)
+        elsif v1.is_a? LcCmx || v2.is_a? LcCmx
+            return complex_div_num(v1,v2) if v1.is_a? LcCmx
+            return complex_div_num(v2,v1)
+        else 
+            val1 = internal.lc_num_to_cr_f(v1)
+            val2 = internal.lc_num_to_cr_f(v2)
+            return Null unless val1 && val2
+            return num2float(Math.atan2(val1,val2).to_f)
+        end
     end
 
     m_atan2 = LcProc.new do |args|
-        args = args.as(T3)
-        arg1 = args[1]
-        arg2 = args[2]
-        val1 = internal.lc_num_to_cr_f(arg1)
-        val2 = internal.lc_num_to_cr_f(arg2)
-        next Null unless val1 && val2
-        next num2float(Math.atan2(val1,val2).to_f)
+        mfun2 atan2
+    end
+
+    def self.lc_sqrt(v : Value)
+        return complex_sqrt(v) if v.is_a? LcCmx
+        val = internal.lc_num_to_cr_f(v)
+        return Null unless val
+        if val < 0
+            return complex_sqrt_num(val)
+        end
+        return num2float(Math.sqrt(val).to_f)
     end
 
     m_sqrt = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        next complex_sqrt(arg) if arg.is_a? LcCmx
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        if val < 0
-            next complex_sqrt_num(val)
-        end
-        next num2float(Math.sqrt(val).to_f)
+        mfun sqrt
+    end
+
+    def self.lc_cbrt(v : Value)
+        val = internal.lc_num_to_cr_f(v)
+        return Null unless val
+        return num2float(Math.cbrt(val).to_f)
     end
 
     m_cbrt = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        next num2float(Math.cbrt(val).to_f)
+        mfun cbrt
     end
 
-    m_nrt = LcProc.new do |args|
-        args = args.as(T3)
-        arg1 = args[1]
-        arg2 = args[2]
-        val1 = internal.lc_num_to_cr_i(arg1)
-        val2 = internal.lc_num_to_cr_f(arg2)
-        next Null unless val1 && val2
+    def self.lc_nrt(v1 : Value,v2 : Value)
+        val1 = internal.lc_num_to_cr_i(v1)
+        return Null unless val1
+        val2 = internal.lc_num_to_cr_f(v2)
+        return Null unless val2
         if val1.even? && val2 < 0
             lc_raise(LcMathError,"(Value out of domain)")
-            next Null
+            return Null
         end
         if val1.is_a? BigInt
             val1 = bigf2flo64(num2bigfloat(val1))
         else 
             val1 = val1.to_f
         end
-        next num2float(val2 ** (1/val1))
+        return num2float(val2 ** (1/val1.to_f))
+    end
+
+    m_nrt = LcProc.new do |args|
+        mfun2 nrt
+    end
+
+    def self.lc_copysign(v1 : Value, v2 : Value)
+        val1 = internal.lc_num_to_cr_f(v1)
+        return Null unless val1
+        val2 = internal.lc_num_to_cr_f(v2)
+        return Null unless val2
+        return num2float(Math.copysign(val1,val2).to_f)
     end
 
     m_copysign = LcProc.new do |args|
-        args = args.as(T3)
-        arg1 = args[1]
-        arg2 = args[2]
-        val1 = internal.lc_num_to_cr_f(arg1)
-        val2 = internal.lc_num_to_cr_f(arg2)
-        next Null unless val1 && val2
-        next num2float(Math.copysign(val1,val2).to_f)
+        mfun2 copysign
+    end
+
+    def self.lc_log10(v : Value)
+        val = internal.lc_num_to_cr_f(v)
+        return Null unless val
+        if val < 0
+            lc_raise(LcMathError,"(Value out of domain)")
+            return Null
+        end
+        return num2float(Math.log10(val).to_f)
     end
 
     m_log10 = LcProc.new do |args|
-        arg = args.as(T2)[1]
-        val = internal.lc_num_to_cr_f(arg)
-        next Null unless val
-        if val < 0
-            lc_raise(LcMathError,"(Value out of domain)")
-            next Null
-        end
-        num2float(Math.log10(val).to_f)
+        mfun log10
+    end
+
+    def self.lc_hypot(v1 : Value, v2 : Value)
+        val1 = internal.lc_num_to_cr_f(v1)
+        return Null unless val1
+        val2 = internal.lc_num_to_cr_f(v2)
+        return Null unless val2
+        return num2float(Math.hypot(val1,val2).to_f)
     end
 
     m_hypot = LcProc.new do |args|
-        args = args.as(T3)
-        arg1 = args[1]
-        arg2 = args[2]
-        val1 = internal.lc_num_to_cr_f(arg1)
-        val2 = internal.lc_num_to_cr_f(arg2)
-        next Null unless val1 && val2
-        num2float(Math.hypot(val1,val2).to_f)
+        mfun2 hypot
     end
 
     MathM = internal.lc_build_internal_module("Math")
