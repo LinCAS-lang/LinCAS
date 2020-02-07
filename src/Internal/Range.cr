2bc99d111f4eddead7bb1f62f700390f82ac9d4a
max-codeware <max.codeware@gmail.com>
2020-02-07 16:07:01 +0100

    Type restriction temporary patch

--- src/Internal/Range.cr
+++ src/Internal/Range.cr
@@ -21,9 +21,6 @@ module LinCAS::Internal
         @right : Intnum = 0
         @inclusive = true
         property left,right,inclusive
-        def to_s
-            return "#{@left}#{@inclusive ? ".." : "..."}#{@right}"
-        end
     end    
 
     macro r_left(range)
@@ -47,10 +44,11 @@ module LinCAS::Internal
         rht = r_right(range)
         inc = inclusive(range) ? 1 : 0
         if lft > rht
-            return lft - rht + inc 
+            size = lft - rht + inc 
         else 
-            return rht - lft + inc 
+            size = rht - lft + inc 
         end
+        return i_to_t(size,IntD) || 0
     end
 
     @[AlwaysInline]
