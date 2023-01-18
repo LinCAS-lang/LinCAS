e2575c654d3b11a8b636430c5c637738bdef2255
Massimiliano Dal Mas <max.codeware@gmail.com>
2023-01-18 10:23:45 +0100

    Fixed copyright

--- src/Exec/VM.cr
+++ src/Exec/VM.cr
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2022 Massimiliano Dal Mas
+# Copyright (c) 2020-2023 Massimiliano Dal Mas
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
