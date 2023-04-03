80fd8c2a8431430b5240b6eb5092daf65b621d78
Massimiliano Dal Mas <max.codeware@gmail.com>
2023-04-03 11:01:29 +0200

    Refactored

--- src/Internal/Number.cr
+++ src/Internal/Number.cr
@@ -15,148 +15,211 @@
 
 module LinCAS::Internal
 
-    alias NumType = LcInt | LcFloat
+  alias NumType = LcInt | LcFloat
 
-    macro positive_num(v)
-        ({{v}}.as(LcNum).val > 0)
+  macro positive_num(v)
+    ({{v}}.as(LcNum).val > 0)
+  end
+
+  macro num2num(v)
+    {{v}}.as(LcNum).val 
+  end
+
+  macro check_num(v)
+    if !({{v}}.is_a? LcNum)
+      lc_raise(lc_type_err,"No implicit conversion of #{lc_typeof({{v}})} into Number")
     end
+  end
 
-    macro num2num(v)
-        {{v}}.as(LcNum).val 
+  abstract class LcNum < LcBase
+  end
+
+  class LcNumI < LcNum
+    def initialize(@val = 0.as(Num)); end
+    property val 
+  end
+
+  @[AlwaysInline]
+  protected def self.num_append(buffer : String_buffer,value :  LcVal)
+    buffer_append(buffer,num2num(value).to_s)
+  end
+
+  @[AlwaysInline]
+  protected def self.num_auto(value : Num)
+    if value.is_a? Float 
+      return num2float(value)
     end
+    return num2int(value)
+  end
 
-    macro check_num(v)
-        if !({{v}}.is_a? LcNum)
-            lc_raise(lc_type_err,"No implicit conversion of #{lc_typeof({{v}})} into Number")
-            return Null 
-        end
+  def self.new_number(klass :  LcVal)
+    klass = klass.as(LcClass)
+    num   = lincas_obj_alloc LcNumI, lc_cast(klass, LcClass)
+    return lc_obj_freeze(num)
+  end
+
+  @[AlwaysInline]
+  def self.lc_number_allocate(klass : LcVal)
+    return new_number(klass)
+  end
+
+  def self.do_coerce(v1 :  LcVal, v2 :  LcVal, error : Bool)
+    if !lc_obj_responds_to? v2,"coerce"
+      if error
+        lc_raise(lc_type_err, "Cant't coerce #{lc_typeof(v2)} into ,#{lc_typeof(v1)}")
+      end
+      return nil
     end
-
-    abstract class LcNum < LcBase
-    end
-
-    class Num_ < LcNum
-        @val = 0.as(Num)
-        property val 
-    end
-
-    @[AlwaysInline]
-    protected def self.num_append(buffer : String_buffer,value :  LcVal)
-        buffer_append(buffer,num2num(value).to_s)
-    end
-
-    @[AlwaysInline]
-    protected def self.num_auto(value : Num)
-        if value.is_a? Float 
-            return num2float(value)
-        end
-        return num2int(value)
-    end
-
-    def self.build_number(klass :  LcVal)
-        klass      = klass.as(LcClass)
-        num = lincas_obj_alloc Num_, lc_cast(klass, LcClass)
-        num.flags |= ObjectFlags::FROZEN 
-        return num.as( LcVal)
-    end
-
-    @[AlwaysInline]
-    def self.lc_number_allocate(klass : LcVal)
-        return build_number(klass)
-    end
-
-    @[AlwaysInline]
-    def self.lc_num_hash(n :  LcVal)
-        return num2int(num2num(n).hash.to_i64)
-    end
-
-    def self.lc_num_coerce(v1 :  LcVal,v2 :  LcVal,method : String)
-        if v1.is_a? NumType && v2.is_a? NumType
-            v1 = num2float(num2num(v1).to_f)
-            v2 = num2float(num2num(v2).to_f)
-            Exec.lc_call_fun(v1,method,v2)
-        else
-            c = internal.coerce(v1,v2).as( LcVal)
-            return Null if c == Null
-            if !(c.is_a? LcArray)
-                lc_raise(lc_type_err,"Coerce must return [x,y]")
-                return Null 
-            end
-            if !(ary_size(c) == 2)
-                lc_raise(lc_type_err,"Coerce must return [x,y]")
-                return Null 
-            end
-            return Exec.lc_call_fun(
-                lc_ary_index(c,num2int(0)),
-                method,
-                lc_ary_index(c,num2int(1))
-            )
-        end 
-    end
-
-    def self.lc_num_gr(n1 :  LcVal, n2 :  LcVal)
-        if n1.is_a? NumType && n2.is_a? NumType
-            return val2bool(num2num(n1) > num2num(n2))
-        else 
-            lc_raise(lc_arg_err,convert(:comparison_failed) % {lc_typeof(n1),lc_typeof(n2)})
-        end
-    end
-
-    def self.lc_num_sm(n1 :  LcVal, n2 :  LcVal)
-        if n1.is_a? NumType && n2.is_a? NumType
-            return val2bool(num2num(n1) < num2num(n2))
-        else 
-            lc_raise(lc_arg_err,convert(:comparison_failed) % {lc_typeof(n1),lc_typeof(n2)})
-        end
-    end
-
-    def self.lc_num_ge(n1 :  LcVal, n2 :  LcVal)
-        if n1.is_a? NumType && n2.is_a? NumType
-            return val2bool(num2num(n1) >= num2num(n2))
-        else 
-            lc_raise(lc_arg_err,convert(:comparison_failed) % {lc_typeof(n1),lc_typeof(n2)})
-        end
-    end
-
-    def self.lc_num_se(n1 :  LcVal, n2 :  LcVal)
-        if n1.is_a? NumType && n2.is_a? NumType
-            return val2bool(num2num(n1) <= num2num(n2))
-        else 
-            lc_raise(lc_arg_err,convert(:comparison_failed) % {lc_typeof(n1),lc_typeof(n2)})
-        end
-    end
-
-    @[AlwaysInline]
-    def self.lc_num_is_zero(num :  LcVal)
-        return lcfalse unless num.is_a? NumType
-        return val2bool(num2num(num) == 0)
-    end
-
-    def self.lc_num_coerce(n1 :  LcVal, n2 :  LcVal)
-        tmp = num2int(0)
-        v1  = lc_num_to_cr_f(n1)
-        return tuple2array(tmp,tmp) unless v1
-        v2  = lc_num_to_cr_f(n2)
-        return tuple2array(tmp,tmp) unless v2
-        return tuple2array(num2float(v2),num2float(v1))
-    end
-
+    ary = Exec.lc_call_fun(v2,"coerce",v1)
     
-
-    def self.init_number
-        @@lc_number = internal.lc_build_internal_class("Number")
-        define_allocator(@@lc_number,lc_number_allocate)
-
-        lc_undef_method(@@lc_number,"defrost")
-
-        define_method(@@lc_number,">",lc_num_gr,            1)
-        define_method(@@lc_number,"<",lc_num_sm,            1)
-        define_method(@@lc_number,">=",lc_num_ge,           1)
-        define_method(@@lc_number,"<=",lc_num_se,           1)
-        define_method(@@lc_number,"zero?",lc_num_is_zero,   0)
-        define_method(@@lc_number,"coerce",lc_num_coerce,   1)
-        define_method(@@lc_number,"hash",lc_num_hash,       0)
+    if !error && ary == Null
+      return nil
     end
+    if !(ary.is_a? LcArray) || !(ary_size(ary) == 2)
+      lc_raise(lc_type_err,"Coerce must return [x,y]")
+    end
+    return ary
+  end 
 
-    
+  def self.lc_num_coerce_bin(v1 :  LcVal, v2 :  LcVal, method : String)
+    if v1.is_a? NumType && v2.is_a? NumType
+      v1 = num2float(num2num(v1).to_f)
+      v2 = num2float(num2num(v2).to_f)
+      Exec.lc_call_fun(v1, method, v2)
+    end
+    # We can safely use #not_nil! because `do_coerce` raises
+    # an exception instead of returning `nil` in this case
+    c = do_coerce(v1,v2, true).not_nil!
+    return Exec.lc_call_fun(
+      lc_ary_index(c, num2int(0)),
+      method,
+      lc_ary_index(c, num2int(1))
+    )
+  end
+
+  def self.lc_num_coerce_cmp(v1 :  LcVal, v2 :  LcVal, method : String)
+    if (c = do_coerce(v1, v2, false))
+      return Exec.lc_call_fun(
+        lc_ary_index(c, num2int(0)),
+        method,
+        lc_ary_index(c, num2int(1))
+      )
+    end
+    return Null
+  end
+
+  def self.lc_num_coerce_rel_op(v1 : LcVal, v2 : LcVal, method : String)
+    if !(c = do_coerce(v1, v2, false))
+      lc_comp_err(v1, v2)
+    else
+      ensure_cmp(
+        Exec.lc_call_fun(
+          lc_ary_index(c, num2int(0)), 
+          method, 
+          lc_ary_index(c, num2int(2))
+        ), 
+        v1, 
+        v2
+      )
+    end
+  end
+
+  @[AlwaysInline]
+  def self.lc_comp_err(v1 : LcVal, v2 : LcVal)
+    lc_raise(lc_arg_err, "Comparison between #{lc_typeof(v1)} and #{lc_typeof(v2)} failed")
+  end
+
+  @[AlwaysInline]
+  def self.ensure_cmp(res : LcVal, v1 : LcVal, v2 : LcVal)
+    if res == Null
+      lc_comp_err(v1, v2)
+    end
+    return res
+  end
+
+  @[AlwaysInline]
+  def self.lc_num_hash(n :  LcVal)
+    return num2int(num2num(n).hash.to_i64)
+  end
+
+  def self.lc_int_or_flo_cmp(n : LcVal, obj : LcVal)
+    if obj.is_a? LcNum
+      x = num2num(n)
+      y = num2num(obj)
+      if x == y
+        return num2int(0)
+      elsif x > y
+        return num2int(1)
+      end
+      return num2int(-1)
+    else
+      return lc_num_coerce_cmp(n, obj, "<=>")
+    end
+  end
+
+  def self.lc_num_gr(n1 :  LcVal, n2 :  LcVal)
+    if n1.is_a? NumType && n2.is_a? NumType
+      return val2bool(num2num(n1) > num2num(n2))
+    else 
+      return lc_num_coerce_rel_op(n1, n2, ">")
+    end
+  end
+
+  def self.lc_num_sm(n1 :  LcVal, n2 :  LcVal)
+    if n1.is_a? NumType && n2.is_a? NumType
+      return val2bool(num2num(n1) < num2num(n2))
+    else 
+      return lc_num_coerce_rel_op(n1, n2, "<")
+    end
+  end
+
+  def self.lc_num_ge(n1 :  LcVal, n2 :  LcVal)
+    if n1.is_a? NumType && n2.is_a? NumType  
+      return val2bool(num2num(n1) >= num2num(n2))
+    else 
+      return lc_num_coerce_rel_op(n1, n2, ">=")
+    end
+  end
+
+  def self.lc_num_se(n1 :  LcVal, n2 :  LcVal)
+    if n1.is_a? NumType && n2.is_a? NumType
+      return val2bool(num2num(n1) <= num2num(n2))
+    else 
+      return lc_num_coerce_rel_op(n1, n2, "<=)")
+    end
+  end
+
+  @[AlwaysInline]
+  def self.lc_num_is_zero(num :  LcVal)
+    return lcfalse unless num.is_a? NumType
+    return val2bool(num2num(num) == 0)
+  end
+
+  def self.lc_num_coerce(n1 :  LcVal, n2 :  LcVal)
+    tmp = num2int(0)
+    v1  = lc_num_to_cr_f(n1)
+    return tuple2array(tmp,tmp) unless v1
+    v2  = lc_num_to_cr_f(n2)
+    return tuple2array(tmp,tmp) unless v2
+    return tuple2array(num2float(v2),num2float(v1))
+  end
+
+  
+
+  def self.init_number
+    @@lc_number = internal.lc_build_internal_class("Number")
+    define_allocator(@@lc_number,lc_number_allocate)
+
+    lc_undef_method(@@lc_number,"defrost")
+
+    define_method(@@lc_number,">",lc_num_gr,            1)
+    define_method(@@lc_number,"<",lc_num_sm,            1)
+    define_method(@@lc_number,">=",lc_num_ge,           1)
+    define_method(@@lc_number,"<=",lc_num_se,           1)
+    define_method(@@lc_number,"zero?",lc_num_is_zero,   0)
+    define_method(@@lc_number,"coerce",lc_num_coerce,   1)
+    define_method(@@lc_number,"hash",lc_num_hash,       0)
+  end
+
+  
 end
