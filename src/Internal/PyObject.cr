ee81dc9aec3c9b9c02eb49eb58de5c2363fc768d
Massimiliano Dal Mas <max.codeware@gmail.com>
2023-09-01 14:31:20 +0200

    Implemented allocation of embedded classes

--- src/Internal/PyObject.cr
+++ src/Internal/PyObject.cr
@@ -15,7 +15,7 @@
 module LinCAS::Internal
 
   class LcPyObject < LcBase
-    getter py_obj
+    property py_obj
 
     def initialize(@py_obj : PyObject* = Pointer(PyObject).null)
     end
@@ -30,7 +30,11 @@ module LinCAS::Internal
     if !obj.is_a? LcPyObject
       lc_raise(lc_type_err, "No implicit conversion of #{lc_typeof(obj)} into PyObject")
     end
-    return obj.as LcPyObject
+    obj = obj.as LcPyObject
+    if obj.py_obj.null?
+      lc_raise(lc_runtime_err, "Uninitialized pyobject detected (#{lc_typeof(obj)})")
+    end
+    return obj
   end
 
   @[AlwaysInline]
@@ -82,21 +86,38 @@ module LinCAS::Internal
     return obj
   end
 
-  def self.new_pyobj(klass :  LcVal)
-    Null
+  def self.lc_pyobject_allocate(klass : LcVal)
+    lincas_obj_alloc LcPyObject, klass.as(LcClass)
+  end
+
+  def self.lc_pyobj_initialize(obj : LcVal, argv : LcVal)
+    klass = class_of(obj)
+    while klass && !klass.type.py_embedded?
+      klass = klass.parent
+    end
+    if !klass
+      lc_raise(lc_runtime_err, "Unable to find embedded python class")
+    end
+    args = pyarg_tuple(nil, argv.as(Ary))
+    pyobj = call_pycallable(klass.not_nil!.namespace.py_obj, args)
+    obj.as(LcPyObject).py_obj = pyobj
+    obj
   end
 
   def self.lc_pyobj_to_s(obj : LcVal)
+    check_pyobject obj
     str = pyobject_to_py_s obj.as(LcPyObject).py_obj
     v = build_string Python.to_cstr str
+    Python.decref str
     return v
-  ensure
-    Python.decref str.not_nil!
   end
 
   def self.init_pyobject
     @@lc_pyobject = lc_build_internal_class("PyObject")
 
+    define_allocator(@@lc_pyobject, lc_pyobject_allocate)
+
+    define_protected_method(@@lc_pyobject, "initialize", lc_pyobj_initialize, -1)
     define_method(@@lc_pyobject, "to_s", lc_pyobj_to_s, 0)
     define_method(@@lc_pyobject, "to_py", lc_obj_self,  0)
   end
