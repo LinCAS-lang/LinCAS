568014817244c490c714f963961699e5536793ec
Massimiliano Dal Mas <max.codeware@gmail.com>
2025-02-18 22:33:30 +0100

    Fixed small issue with interpolated strings

--- src/Semantic/Parser.cr
+++ src/Semantic/Parser.cr
@@ -1137,7 +1137,8 @@ module LinCAS
 
       if has_interpolation
         chunks = combine_interpolation_chunks(chunks, delimiter_t)
-        result = StringLiteral.new chunks, interpolated: true
+        int = chunks.size > 1 || chunks.first.is_a? String
+        result = StringLiteral.new chunks, interpolated: int
       else
         chunk = combine_chunks(chunks, delimiter_t)
         result = StringLiteral.new chunk, interpolated: false
